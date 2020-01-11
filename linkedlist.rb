434c0c13a8d71abd443cce7c87d9bb6c8dd3c0e7
Trey Guy <tguy728@gmail.com>
2020-01-11 18:41:44 +0000

    FC

---
+++ linkedlist.rb
@@ -0,0 +1,12 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
